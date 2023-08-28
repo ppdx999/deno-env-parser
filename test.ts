d479045ffeee88b58f8775053adabf37333d8c4e
ppdx999 <ppdx999@gmail.com>
2023-08-28 16:13:08 +0900

    rename a func

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.198.0/assert/mod.ts";
-import { Env, parse, parseKV } from "./mod.ts";
+import { Env, parse, parseToRecord } from "./mod.ts";
 
 const source = `# this is a comment
 str="value"
@@ -43,6 +43,6 @@ Deno.test("parseKV", () => {
     str_start_zero: "0123",
   };
 
-  const actual = parseKV(source);
+  const actual = parseToRecord(source);
   assertEquals(actual, expected);
 });
